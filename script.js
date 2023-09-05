6d5944f9d37cede99284ebbf35882656773aacd3
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-09-05 10:39:39 -0400

    add dialog box and functionality to + button

---
+++ script.js
@@ -0,0 +1,7 @@
+const dialogBox = document.getElementById("new-book-form");
+const showDialogButton = document.getElementById("showDialog");
+
+showDialogButton.addEventListener("click", (e) => {
+    dialogBox.showModal();
+})
+
