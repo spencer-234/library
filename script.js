d15cee3579f910cd3d6ba51989e7d788fb0599d1
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-09-06 12:09:01 -0400

    add library array to script

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dialogBox = document.getElementById("bookDialog");
 const showDialogButton = document.getElementById("showDialog");
 const cancelBtn = document.getElementById("cancelBtn");
+const myLibrary = [];
 
 showDialogButton.addEventListener("click", (e) => {
     dialogBox.showModal();
