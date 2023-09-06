c197da7a38b020734c6f0d5d4f6a49c02c8e513d
spencer-234 <133820052+spencer-234@users.noreply.github.com>
2023-09-06 12:28:41 -0400

    add function to submit book into array

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const dialogBox = document.getElementById("bookDialog");
 const showDialogButton = document.getElementById("showDialog");
 const cancelBtn = document.getElementById("cancelBtn");
 const myLibrary = [];
+const submittedForm = document.querySelector(".book-form")
 
 showDialogButton.addEventListener("click", (e) => {
     dialogBox.showModal();
@@ -11,9 +12,26 @@ cancelBtn.addEventListener("click", (e) => {
     dialogBox.close();
 });
 
-function book(title, author, pages, haveRead) {
+submittedForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let title = document.getElementById("title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let haveRead = document.getElementById("haveRead").value;
+    let book = new Book(title, author, pages, haveRead);
+    addBookToLibrary(book);
+    dialogBox.close();
+})
+
+console.log(myLibrary);
+
+function Book(title, author, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
 };
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
